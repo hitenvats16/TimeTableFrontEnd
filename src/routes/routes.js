85bf3c810fe156a694d055e2652a89064894b0a7
itspkjha <86141517+itspkjha@users.noreply.github.com>
2022-09-09 08:17:49 +0530

    Add files via upload

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,10 +1,12 @@
 import { Routes,Route } from "react-router-dom";
 import AddData from "../modules/presentation/pages/addData";
 import Home from "../modules/presentation/pages/Home";
+import LandingPage from "../modules/presentation/pages/LandingPage";
 
 function Router(){
     return(
         <Routes>
+            <Route path="" element={<LandingPage/>} />
             <Route path="add" element={<AddData/>}/>
             <Route path="generate" element={<Home/>}/>
         </Routes>
